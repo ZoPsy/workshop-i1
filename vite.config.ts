0b6743eabc921dbaf9b6de2e5aaedfaec2a696f8
Germain <germain.leignel@gmail.com>
2023-10-10 11:45:09 +0200

    pwa conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		sveltekit(),
+		purgeCss(),
+		VitePWA({
+			registerType: 'autoUpdate'
+		}),
+	]
 });
