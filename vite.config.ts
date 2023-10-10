a07dcbc745ffb9152915ed63c73a23096f0cbd3e
Germain <germain.leignel@gmail.com>
2023-10-10 13:06:23 +0200

    Revert "Merge pull request #11 from ZoPsy/pwa"
    This reverts commit bb500c33e25cf074b3da46c51b2fe58c39358a61, reversing
    changes made to 7e80cbdae69821c2eaa2aa382e6a7bb562fe8e2d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		purgeCss(),
-		VitePWA({
-			registerType: 'autoUpdate'
-		}),
-	]
+	plugins: [sveltekit(), purgeCss()]
 });
