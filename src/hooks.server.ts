32c179306fc30935afa0eda0302f79fb291a190d
Germain <germain.leignel@gmail.com>
2023-10-10 10:31:28 +0200

    postgres connection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,14 @@
+import { DATABASE_URL } from '$env/static/private';
 import type { Handle } from '@sveltejs/kit';
+import postgres from 'postgres';
 
 export const handle: Handle = async ({ event, resolve }) => {
+
+    const sql = postgres(DATABASE_URL);
+
+    event.locals = {
+        sql: sql
+    };
 
     const response = await resolve(event);
     return response;
