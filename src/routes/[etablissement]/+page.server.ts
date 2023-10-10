8583ee7c738cc0044f88907341f23942bf0521f6
Germain <germain.leignel@gmail.com>
2023-10-10 10:31:37 +0200

    example

--- src/routes/[etablissement]/+page.server.ts
+++ src/routes/[etablissement]/+page.server.ts
@@ -1,10 +1,21 @@
-import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ locals, params }) => {
 
-    throw error(404, {
-        message: 'Not found',
-    });
-    return {};
+    const sql = locals.sql;
+    const etablissementExpected = params.etablissement;
+
+    const etablissement = await sql`
+        SELECT
+        name
+        FROM establishment
+        WHERE name = ${etablissementExpected}
+    `;
+
+    if (!etablissement || etablissement.length === 0) {
+        throw error(404, 'Etablissement not found');
+    }
+
+    return { etablissement };
 }) satisfies PageServerLoad;
