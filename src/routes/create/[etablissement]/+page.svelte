a5c3c06e2ac565a6ca8afb482fe4efae44fe58be
Germain <germain.leignel@gmail.com>
2023-10-11 15:06:38 +0200

    push

--- src/routes/create/[etablissement]/+page.svelte
+++ src/routes/create/[etablissement]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { page } from '$app/stores';
-	import QRCode from '@castlenine/svelte-qrcode';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 	let base_url = '';
@@ -9,9 +9,34 @@
 		base_url = pageData.url.origin;
 	});
 
-    let qrCodes = []
+	let qrCodeUrls: string[] = [];
+	let qrCodeImages: string[] = [];
+
+	for (let i = 0; i < 4; i++) {
+		qrCodeUrls.push(`https://workshop-i1-production.up.railway.app/${data.establishment}/${i}`);
+	}
+
+	const generateQRCode = async (url: string) => {
+		const response = await fetch(
+			`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=150x150`
+		);
+		if (response.ok) {
+			const blob = await response.blob();
+			const imageUrl = URL.createObjectURL(blob);
+			qrCodeImages.push(imageUrl);
+		}
+	};
+
+	onMount(() => {
+		qrCodeUrls.forEach((url) => generateQRCode(url));
+	});
 </script>
 
 <div class="container">
-	<QRCode content="https://workshop-i1-production.up.railway.app/epsi/1" />
+	{#each qrCodeImages as qrCodeImage, index}
+		<div class="qr-item">
+			<img src={qrCodeImage} alt="QR Code" />
+			<a href={qrCodeImage} download={`QRCode_${index + 1}.png`}>Download QR Code {index + 1}</a>
+		</div>
+	{/each}
 </div>
