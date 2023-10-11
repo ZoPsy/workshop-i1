a41942950b81fd58d56914e3a343a13e357bcb34
Germain <germain.leignel@gmail.com>
2023-10-11 09:33:35 +0200

    clean

--- src/routes/[etablissement]/[question_id]/+page.svelte
+++ src/routes/[etablissement]/[question_id]/+page.svelte
@@ -36,7 +36,7 @@
 	{:else}
 		<div in:slide={{ duration: 300, axis: 'x' }} class="content-container">
 			<div class="flex flex-col gap-10 items-center">
-				<span>Scénario N°{data.question.number} sur 4</span>
+				<span>Scénario N°{data.question.number}</span>
 				<span>{data.question.explanation}</span>
 				<div class="flex flex-col gap-5 w-full">
 					{#each data.answers as answer}
