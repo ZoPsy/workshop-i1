75875007bae92e936e1a1b3e7e87bb16539a0370
Edward Brunetiere <57622377+P4tt4te@users.noreply.github.com>
2023-10-12 10:57:26 +0200

    Revert "add first version on production"

--- src/routes/[etablissement]/end/+page.server.ts
+++ src/routes/[etablissement]/end/+page.server.ts
@@ -1,24 +1,8 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params, locals, cookies }) => {
+export const load = (async ({ params }) => {
 	console.log(params.etablissement);
-
-	const sql = locals.sql;
-	const username = cookies.get('username');
-
-	const user = await sql`
-                SELECT
-                score
-                FROM player
-                WHERE name = ${username}
-                AND establishment_id = (SELECT establishment_id FROM establishment WHERE name = ${params.etablissement} )
-            `;
-
-			console.log(user)
-			
-
 	return {
-		etablissement: params.etablissement,
-		score: user[0].score
+		etablissement: params.etablissement
 	};
 }) satisfies PageServerLoad;
