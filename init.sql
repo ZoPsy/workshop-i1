e2d948dce0f965b783511b8a42b89fd8bc78fa3c
HugoFerry <72927744+HugoFerry@users.noreply.github.com>
2023-10-12 09:00:21 +0200

    Update init.sql
    Toutes les questions et réponses

--- init.sql
+++ init.sql
@@ -55,6 +55,30 @@ VALUES (
         'Je suis l''endroit où l''on se retire pour un moment de réflexion, Un lieu d''intimité où chacun devrait trouver sa protection. Cependant, parfois, des ombres s''insinuent en ces lieux, Là où le respect devrait régner, des actes déplacés peuvent avoir lieu. Le harcèlement se cache dans l''obscurité des mots et des gestes, Dans ces endroits, un lieu de harcèlement où la douleur s''atteste. Sensibiliser est notre quête, révéler ces abris, Pour qu''aucune obscurité n''y cache actes impolis.',
         2
     );
+INSERT INTO questions (
+    question_text,
+    explanation,
+    enigme,
+    number,
+)
+VALUES (
+           'Cette situation est-elle un mythe ou une réalité ?',
+           'Si quelqu''un est réellement victime de harcèlement sexuel au travail, il le signalera immédiatement',
+           'Dans un coin de l''entreprise, discrète et petite, Je sers l''énergie, je réveille les âmes endormies. L''eau chaude coule, le parfum du café embaume, Mais dans les couloirs, parfois, règne un autre royaume. Des regards de trop près, des mots de travers, Dans ces moments d''insouciance naissent parfois des revers. Mon lieu est sûr, mais dans l''ombre parfois se glisse, Ce qu''on ne devrait pas tolérer, ce qui fait mal, ce qui blesse. Où suis-je donc, cet endroit souvent animé, Où règne la convivialité, mais où peut se cacher la réalité ?',
+           3,
+       );
+INSERT INTO questions (
+    question_text,
+    explanation,
+    enigme,
+    number,
+)
+VALUES (
+           'Ce comportement correspond à :',
+           'Il y a deux semaines, Sarah a été confrontée à des commentaires inappropriés de la part de son manager. Lundi dernier, ces remarques sur son apparence ont persisté, et aujourd''hui encore, il a fait des commentaires déplacés.',
+           '',
+           4,
+       );
 INSERT INTO establishment_question (establishment_id, question_id)
 VALUES (1, 1),
     (1, 2);
@@ -138,3 +162,63 @@ VALUES (
             where explanation = 'Vous êtes en réunion d''équipe avec vos collègues. Lorsque vous commencez à présenter votre projet, l''un de vos collègues, Mark, fait des commentaires sur votre apparence physique et, exceptionnellement dit : "Tu es vraiment sexy dans cette tenue, ça fait plaisir à voir."'
         )
     );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Mythe',
+           true,
+           (
+               select question_id
+               from questions
+               where explanation = 'Si quelqu''un est réellement victime de harcèlement sexuel au travail, il le signalera immédiatement'
+           )
+       );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Réalité',
+           false,
+           (
+               select question_id
+               from questions
+               where explanation = 'Si quelqu''un est réellement victime de harcèlement sexuel au travail, il le signalera immédiatement'
+           )
+       );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Séduction ou drague',
+           false,
+           (
+               select question_id
+               from questions
+               where explanation = 'Il y a deux semaines, Sarah a été confrontée à des commentaires inappropriés de la part de son manager. Lundi dernier, ces remarques sur son apparence ont persisté, et aujourd''hui encore, il a fait des commentaires déplacés.'
+           )
+       );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Agression sexuelle',
+           false,
+           (
+               select question_id
+               from questions
+               where explanation = 'Il y a deux semaines, Sarah a été confrontée à des commentaires inappropriés de la part de son manager. Lundi dernier, ces remarques sur son apparence ont persisté, et aujourd''hui encore, il a fait des commentaires déplacés.'
+           )
+       );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Harcèlement sexuel',
+           true,
+           (
+               select question_id
+               from questions
+               where explanation = 'Il y a deux semaines, Sarah a été confrontée à des commentaires inappropriés de la part de son manager. Lundi dernier, ces remarques sur son apparence ont persisté, et aujourd''hui encore, il a fait des commentaires déplacés.'
+           )
+       );
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES (
+           'Agissement sexiste',
+           false,
+           (
+               select question_id
+               from questions
+               where explanation = 'Il y a deux semaines, Sarah a été confrontée à des commentaires inappropriés de la part de son manager. Lundi dernier, ces remarques sur son apparence ont persisté, et aujourd''hui encore, il a fait des commentaires déplacés.'
+           )
+       );
