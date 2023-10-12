e660c07a184a5e02a6183461a314a0fb1f27e481
Germain <germain.leignel@gmail.com>
2023-10-12 10:01:05 +0200

    kinda pdf

--- src/routes/create/[etablissement]/+page.svelte
+++ src/routes/create/[etablissement]/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageData } from './$types';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 	export let data: PageData;
 	let base_url = '';
@@ -13,9 +14,8 @@
 	let qrCodeImages: string[] = [];
 
 	for (let i = 1; i < 5; i++) {
-		qrCodeUrls.push(`https://workshop-i1-production.up.railway.app/${data.establishment}/${i}`);
+		qrCodeUrls.push(`https://workshop-i1-dev.up.railway.app/${data.establishment}/${i}`);
 	}
-	console.log(qrCodeUrls)
 
 	const generateQRCode = async (url: string) => {
 		const response = await fetch(
@@ -24,9 +24,35 @@
 		if (response.ok) {
 			const blob = await response.blob();
 			const imageUrl = URL.createObjectURL(blob);
+			qrCodeImages.push(imageUrl); // Store the generated QR code image URL
 			return imageUrl;
 		}
 	};
+
+	let pdfLink: string = '';
+	type Alignment = 'left' | 'center' | 'right';
+
+	onMount(async () => {
+		const pdfmake = await import('pdfmake/build/pdfmake.js');
+		const fonts = await import('pdfmake/build/vfs_fonts.js');
+
+		pdfmake.vfs = fonts.pdfMake.vfs;
+
+		const docDefinition: TDocumentDefinitions = {
+			content: qrCodeImages.map((image) => ({
+				image,
+				width: 200,
+				alignment: 'center',
+				margin: [0, 200]
+			})),
+			pageSize: 'A4'
+		};
+
+		const pdfDocGenerator = pdfmake.createPdf(docDefinition);
+		pdfDocGenerator.getDataUrl((dataUrl: string) => {
+			pdfLink = dataUrl;
+		});
+	});
 </script>
 
 <div class="container grid grid-cols-3 gap-4 mt-8">
@@ -35,10 +61,18 @@
 		<div class="flex flex-col items-center">
 			{#await generateQRCode(url)}
 				<p>Loading qr code</p>
-			{:then image} 
-				<img src="{image}" alt="url" class="w-32 h-32 mb-2">
-				<a href="{image}" download class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Download</a>
+			{:then image}
+				<img src={image} alt="url" class="w-32 h-32 mb-2" />
+				<a href={image} download class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+					>Download</a
+				>
 			{/await}
 		</div>
 	{/each}
+	<!-- Button to download the entire PDF -->
+	<a
+		href={pdfLink}
+		download="QRCodes.pdf"
+		class="mt-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Download All as PDF</a
+	>
 </div>
