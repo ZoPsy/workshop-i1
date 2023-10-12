e3e4b752191779dc36ce62d6da3370aea7bee56b
Germain <germain.leignel@gmail.com>
2023-10-12 09:51:53 +0200

    hh

--- src/routes/create/[etablissement]/+page.svelte
+++ src/routes/create/[etablissement]/+page.svelte
@@ -12,31 +12,33 @@
 	let qrCodeUrls: string[] = [];
 	let qrCodeImages: string[] = [];
 
-	for (let i = 0; i < 4; i++) {
+	for (let i = 1; i < 5; i++) {
 		qrCodeUrls.push(`https://workshop-i1-production.up.railway.app/${data.establishment}/${i}`);
 	}
+	console.log(qrCodeUrls)
 
 	const generateQRCode = async (url: string) => {
 		const response = await fetch(
-			`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=150x150`
+			`https://api.qrserver.com/v1/create-qr-code/?data=${url}&size=300x300`
 		);
 		if (response.ok) {
 			const blob = await response.blob();
 			const imageUrl = URL.createObjectURL(blob);
-			qrCodeImages.push(imageUrl);
+			return imageUrl;
 		}
 	};
-
-	onMount(() => {
-		qrCodeUrls.forEach((url) => generateQRCode(url));
-	});
 </script>
 
-<div class="container">
-	{#each qrCodeImages as qrCodeImage, index}
-		<div class="qr-item">
-			<img src={qrCodeImage} alt="QR Code" />
-			<a href={qrCodeImage} download={`QRCode_${index + 1}.png`}>Download QR Code {index + 1}</a>
+<div class="container grid grid-cols-3 gap-4 mt-8">
+	<h3 class="col-span-3 mb-4">QR CODES</h3>
+	{#each qrCodeUrls as url}
+		<div class="flex flex-col items-center">
+			{#await generateQRCode(url)}
+				<p>Loading qr code</p>
+			{:then image} 
+				<img src="{image}" alt="url" class="w-32 h-32 mb-2">
+				<a href="{image}" download class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Download</a>
+			{/await}
 		</div>
 	{/each}
 </div>
